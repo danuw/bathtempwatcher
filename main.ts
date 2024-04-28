3aefcd26c8b5c83c0e57e8804919d9493e5fa282
Dan Benitah <bdanuw@gmail.com>
2024-04-28 18:32:45 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+let ThresholdTemp = 0
+ThresholdTemp += 37
+basic.forever(function () {
+    led.plotBarGraph(
+    DS18B20.TemperatureNumber(DS18B20.pin.pin0),
+    100
+    )
+    basic.showString(DS18B20.TemperatureString(DS18B20.pin.pin0))
+    basic.pause(2000)
+    if (DS18B20.TemperatureNumber(DS18B20.pin.pin0) <= ThresholdTemp) {
+        music.play(music.tonePlayable(988, music.beat(BeatFraction.Double)), music.PlaybackMode.InBackground)
+        music.play(music.tonePlayable(988, music.beat(BeatFraction.Double)), music.PlaybackMode.InBackground)
+    }
+})
